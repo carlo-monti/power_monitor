d294b1135e88151d34093074d09a808289ceefe0
Carlo Monti <79301372+carlo-monti@users.noreply.github.com>
2023-08-23 09:54:16 +0200

    Update clock_and_power_monitor.ino

--- clock_and_power_monitor.ino
+++ clock_and_power_monitor.ino
@@ -30,25 +30,18 @@ void setup(void)
 {
   Serial.begin(115200);
   while (!Serial) {
-      // will pause Zero, Leonardo, etc until serial console opens
       delay(1);
   }
   if(!display.begin(SSD1306_SWITCHCAPVCC)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    while(1);
   }
   Serial.println("Hello!");
-  // Initialize the INA219.
-  // By default the initialization will use the largest range (32V, 2A).  However
-  // you can call a setCalibration function to change this range (see comments).
   if (! ina219.begin()) {
     Serial.println("Failed to find INA219 chip");
     while (1) { delay(10); }
   }
-  // To use a slightly lower 32V, 1A range (higher precision on amps):
-  //ina219.setCalibration_32V_1A();
-  // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
-  //ina219.setCalibration_16V_400mA();
+  ina219.setCalibration_16V_400mA(); // highest precision on current
   Serial.println("Measuring voltage and current with INA219 ...");
 }
 
